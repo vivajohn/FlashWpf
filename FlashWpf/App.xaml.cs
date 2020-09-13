0344facc403bcab2e3f700e3e61667e206dc6a48
John Glowinski <hanscdn@gmail.com>
2020-09-13 12:55:56 +0200

    Changed Azure startup

--- FlashWpf/App.xaml.cs
+++ FlashWpf/App.xaml.cs
@@ -17,6 +17,9 @@ namespace FlashWpf
     {
         public App()
         {
+            Cosmos.Endpoint = Secret.Endpoint;
+            Cosmos.AuthKey = Secret.AuthKey;
+
             var serviceCollection = new ServiceCollection();
             ConfigureServices(serviceCollection);
             ServiceLocator.Init(serviceCollection.BuildServiceProvider());
